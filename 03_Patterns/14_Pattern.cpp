c23740798fbe414abc00347c4f11bb5a2e87741d
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-25 23:37:28 +0530

    Pattern-14

---
+++ 03_Patterns/14_Pattern.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin >> ch;
+    for (char i = 'A'; i <= ch; i++)
+    {
+        for (char j = 'A'; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
