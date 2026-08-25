3fefa6e18ef39616a11a287bc996d2deb141cd65
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-26 00:02:33 +0530

    Check for prime numbers

---
+++ 05_Know_Basic_Maths/06_check_for_prime_number.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number: ";
+    cin >> n;
+    cout << endl;
+    int count = 0;
+    if (n < 2)
+    {
+        cout << n << " is not a prime number" << endl;
+        return 0;
+    }
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+            break;
+        }
+    }
+    if (count == 0)
+    {
+        cout << n << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << n << " is not a prime number." << endl;
+    }
+    return 0;
+}
