e1f9b1238dce9057f7adaaf04753c66e594087b2
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-25 23:26:53 +0530

    Pattern-9

---
+++ 03_Patterns/09_Pattern.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i * 2; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = i; j < n - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i * 2; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
