b758f4db783c4cf19d68cbbf3ec8c42f917e3179
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-26 00:01:13 +0530

    Print all divisors

---
+++ 05_Know_Basic_Maths/05_print_all_divisors.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int n;
+    cout << "enter the number: ";
+    cin >> n;
+    cout << endl;
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if (n / i != i)
+            {
+                v.push_back(n / i);
+            }
+        }
+    }
+    sort(v.begin(), v.end());
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << ",";
+    }
+    return 0;
+}
