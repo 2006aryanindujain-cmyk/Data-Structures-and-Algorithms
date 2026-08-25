ca8ae9a0be58daedfe38476682cb53b276c6e319
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-25 23:25:36 +0530

    Pattern 8

---
+++ 03_Patterns/08_Pattern.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i = 4; i >= 0; i--)
+    {
+        for (int j = i; j < 5 - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i * 2; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
