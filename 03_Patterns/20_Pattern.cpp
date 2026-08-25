b28a23dfbe1439417d6a13c2934021824744ef4f
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-25 23:46:05 +0530

    Pattern-20

---
+++ 03_Patterns/20_Pattern.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int stars;
+    int spaces;
+    for (int i = 1; i < 2 * n; i++)
+    {
+        if (i <= n)
+        {
+            stars = i;
+        }
+        else
+        {
+            stars = 2 * n - i;
+        }
+        for (int j = 0; j < stars; j++)
+        {
+            cout << "*";
+        }
+        if (i <= n)
+        {
+            spaces = 2 * n - 2 * i;
+        }
+        else
+        {
+            spaces = 2 * i - 2 * n;
+        }
+        for (int j = 0; j < spaces; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
