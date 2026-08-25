08a07c1f0619277198bad5774eae260896b052ec
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-25 22:54:52 +0530

    For loop in C++

---
+++ 01_Things_to_know_in_C++/06_For_Loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    // simple for loop: print numbers 0 to 4
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+    return 0;
+}
