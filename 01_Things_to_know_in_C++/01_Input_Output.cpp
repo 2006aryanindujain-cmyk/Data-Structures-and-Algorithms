17232f31993ede3e9eb7cd3a0146524898caa688
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-28 01:29:59 +0530

    Input and Output in C++

--- 01_Things_to_know_in_C++/01_Input_Output.cpp
+++ 01_Things_to_know_in_C++/01_Input_Output.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <string>
+// Libraries provide pre-build functions and tools.
+// #include<iostream -> used for input/output.
+// #include<math.h> -> used for mathematical functions.
+// To print output, we use cout.
+// Use \n or endl to print on new lines.
+// \n -> insert a new line(faster).
+// endl -> inserts a new line and flushes the output buffer (slower).
+// cin is used to take input from the user.
 
+#include <iostream>
+using namespace std;
 int main()
 {
-    int number;
-    std::string name;
-
-    // Input from the user
-    std::cout << "Enter a number: ";
-    std::cin >> number;
-
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-
-    // Output to the console
-    std::cout << "You entered number: " << number << "\n";
-    std::cout << "Hello, " << name << "!\n";
-
+    cout << "Hello World!" << endl;
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x is " << x << " and y is " << y << endl;
     return 0;
 }
+
+// Instead of including libraries one by one, we can use #include<bits/stdc++.h>. It includes almost all standard libraries at once.
