efb17be7d471dd54ecc342628210205b61f79e3c
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-28 16:01:45 +0530

    Arrays and strings in C++

--- 01_Things_to_know_in_C++/05_What_are_arrays_and_strings.cpp
+++ 01_Things_to_know_in_C++/05_What_are_arrays_and_strings.cpp
@@ -1,16 +1,15 @@
-// Arrays and strings in C++
+// An array is a linear data structure in which we store data and perform any operation, we can randomly access
+// data in an array(with the help of it index value).
+// An array is a collection of similar type of elements(homogeneous elements) that have contiguous memory location, i.e., one after another.
+// Arrays store the related information in adjacent memory blocks.
+// Syntax of array :- datatype arrayName[arraySize];
+// In arrays, memory is allocated instantly.
+// In array, insertion at end takes constant time O(1).
+// In array , removing at end takes constant time O(1).
 
-// An array is a fixed-size collection of elements of the same type.
-// Example: int numbers[5]; declares an array of 5 integers.
-// The size of the array is fixed at compile time, and elements are
-// accessed using an index starting from 0, e.g. numbers[0].
-
-// A string in C++ can be represented using a C-style character array
-// or the std::string class from the standard library.
-// C-style string example: char name[] = "Alice"; is an array of chars
-// terminated by a null character '\0'.
-// std::string example: std::string text = "Hello"; is easier to use
-// and supports many string operations like concatenation and length.
+// Strings are like a series of characters stored in a specified order. Each character in a string is assigned an index,
+// starting from 0. This means the first character is at index 0, the second character at index 1 and so on.
+// Strings are terminated by a null character '\0'.
 
 // code to find length of a string
 // #include <bits/stdc++.h>
