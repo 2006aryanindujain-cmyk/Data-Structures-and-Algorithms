7c92737e1891144cc602dc812781392c53a262bf
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-28 17:54:39 +0530

    Pattern-12

--- 03_Patterns/12_Pattern.cpp
+++ 03_Patterns/12_Pattern.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter the number: ";
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        int spaces = 8 - 2 * i;
+        int spaces = 2 * (n - i);
         for (int j = 1; j <= i; j++)
         {
             cout << j;
