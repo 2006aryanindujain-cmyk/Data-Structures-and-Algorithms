5c9d95328bc97eddbc53721ba730ac75351a74c8
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-28 17:37:33 +0530

    Pattern-2

--- 03_Patterns/02_Pattern.cpp
+++ 03_Patterns/02_Pattern.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main()
 {
-    for (int i = 0; i <= 4; i++)
+    int n;
+    cout << "Enter the number:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
         {
