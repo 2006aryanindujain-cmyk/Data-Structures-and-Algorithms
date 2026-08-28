c0b7e3a855bafc7ed4b3e4d7993b068ff528a3dd
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-29 01:35:20 +0530

    Print 1 to N using recursion

--- 01_Learn_the_Basics/06_Learn_Basic_Recursion/03_Print_1_to_N_Using_Recursion.cpp
+++ 01_Learn_the_Basics/06_Learn_Basic_Recursion/03_Print_1_to_N_Using_Recursion.cpp
@@ -1,23 +1,67 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print(int n, int i)
+// {
+//     if (i > n)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         cout << i << endl;
+//         i++;
+//     }
+//     print(n, i);
+// }
+// int main()
+// {
+//     int i = 1;
+//     int n;
+//     cout << "Enter the number:";
+//     cin >> n;
+//     print(n, i);
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print(int i, int n)
+// {
+//     if (i > n)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         cout << i << endl;
+//     }
+//     print(i + 1, n);
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number:";
+//     cin >> n;
+//     print(1, n);
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-void print(int n, int i)
+void print(int i, int n)
 {
-    if (i > n)
+    if (i < 1)
     {
         return;
     }
     else
     {
+        print(i - 1, n);
         cout << i << endl;
-        i++;
     }
-    print(n, i);
 }
 int main()
 {
-    int i = 1;
     int n;
     cout << "Enter the number:";
     cin >> n;
-    print(n, i);
+    print(n, n);
 }
