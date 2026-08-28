7fea1fefa2708522d69c5237f1fb858f7c2585a7
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-29 01:24:37 +0530

    Print N to 1 using recursion

---
+++ 01_Learn_the_Basics/06_Learn_Basic_Recursion/04_Print_N_to_1_Using_Recursion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    else
+    {
+        cout << n << endl;
+        n--;
+    }
+    print(n);
+}
+int main()
+{
+    int n;
+    cout << "Enter the number:";
+    cin >> n;
+    print(n);
+}
