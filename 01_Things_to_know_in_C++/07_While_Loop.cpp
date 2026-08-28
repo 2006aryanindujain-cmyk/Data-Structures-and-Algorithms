37181ad487946b244c258cf196a6d8c34fe0a1e8
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-28 16:10:39 +0530

    While loop in C++

--- 01_Things_to_know_in_C++/07_While_Loop.cpp
+++ 01_Things_to_know_in_C++/07_While_Loop.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+// Used to repeat a block of code as long as a given condition is true. Used when the number of iterations is not known in advance.
+// do-while loop is similar to while loop, but the code block executes at least once before checking the condition(condition
+// checked after the execution).
+
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    // While Loop Example
     cout << "While Loop Example:" << endl;
     int i = 1;
     while (i <= 5)
@@ -11,7 +14,6 @@ int main()
         i++;
     }
     cout << "\nDo-While Loop Example:" << endl;
-    // Do-While Loop Example
     int j = 1;
     do
     {
